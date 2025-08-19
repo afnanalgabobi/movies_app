576cee9aad9ea4c06e34e387e363640b9d8df9eb
aliaaemad312 <aliaa.emad312@gmail.com>
2025-08-20 00:50:42 +0300

    + Added Custom Elevated Button.

---
+++ lib/ui/widgets/custom_elevated_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/utils/app_styles_roboto.dart';
+import '../../utils/app_colors.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  VoidCallback onPressed;
+  String text;
+  Color? backgroundColor;
+  TextStyle? textStyle;
+  Color? borderColor;
+  bool icon;
+  Widget? iconWidget;
+  MainAxisAlignment? mainAxisAlignment;
+
+  CustomElevatedButton({super.key,
+    required this.onPressed,
+    required this.text,
+    this.backgroundColor=AppColors.yellowColor,
+    this.textStyle,
+    this.borderColor,
+    this.icon=false,
+    this.iconWidget,
+    this.mainAxisAlignment= MainAxisAlignment.start
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var height=MediaQuery.of(context).size.height;
+    var width=MediaQuery.of(context).size.width;
+    return ElevatedButton(
+        onPressed: (){
+          onPressed();
+        },
+        style: ElevatedButton.styleFrom(
+            elevation: 0,
+            padding: EdgeInsets.symmetric(vertical:height*0.02 ),
+            backgroundColor: backgroundColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+                side: BorderSide(
+                    color: borderColor??AppColors.yellowColor
+                )
+            )
+        ),
+        child: icon ?
+        Row(
+          mainAxisAlignment: mainAxisAlignment!,
+          children: [
+            iconWidget!,
+            Padding(
+              padding:  EdgeInsets.symmetric(horizontal: width*0.02),
+              child: Text(text,
+                style: textStyle??AppStylesRoboto.regular16Black,),
+            )
+
+          ],
+        ):
+        Text(text,
+          style: textStyle??AppStylesRoboto.regular16Black,)
+    );
+  }
+}
