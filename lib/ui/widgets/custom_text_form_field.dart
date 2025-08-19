d960b6d436ee8f33b303cb6af444f4614457f459
aliaaemad312 <aliaa.emad312@gmail.com>
2025-08-20 00:27:35 +0300

    + Added Custom Text Form Field.

---
+++ lib/ui/widgets/custom_text_form_field.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/utils/app_styles_roboto.dart';
+import '../../utils/app_colors.dart';
+
+typedef OnValidator= String? Function(String?)?;
+class CustomTextFormField extends StatelessWidget {
+  Color colorBorderSide;
+  String? hintText;
+  TextStyle? hintStyle;
+  String? labelText;
+  TextStyle? labelStyle;
+  Widget? prefixIcon;
+  Widget? suffixIcon;
+  OnValidator validator;
+  TextEditingController controller;
+  TextInputType keyboardType;
+  bool obscureText;
+  String obscuringCharacter;
+  int? maxLines;
+  Color fillColor;
+
+  CustomTextFormField({super.key,
+    this.colorBorderSide=AppColors.transparentColor,
+    this.fillColor=AppColors.grayColor,
+    this.hintText,
+    this.hintStyle,
+    this.labelText,
+    this.labelStyle,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.validator,
+    required this.controller,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.obscuringCharacter='.',
+    this.maxLines
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var height=MediaQuery.of(context).size.height;
+    var width=MediaQuery.of(context).size.width;
+    return TextFormField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: fillColor,
+          enabledBorder: builtDecorationBorder(colorBorderSide: colorBorderSide),
+          focusedBorder: builtDecorationBorder(colorBorderSide: colorBorderSide),
+          errorBorder: builtDecorationBorder(colorBorderSide: AppColors.redColor),
+          focusedErrorBorder: builtDecorationBorder(colorBorderSide: AppColors.redColor),
+          errorStyle: AppStylesRoboto.regular14White.copyWith(
+              color: AppColors.redColor
+          ),
+          hintText: hintText,
+          hintStyle: hintStyle?? AppStylesRoboto.regular16White,
+          labelText: labelText,
+          labelStyle: hintStyle?? AppStylesRoboto.regular16White,
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon
+      ),
+      maxLines: maxLines ?? 1,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      obscuringCharacter: obscuringCharacter,
+      controller: controller,
+      validator: validator,
+    );
+  }
+  OutlineInputBorder builtDecorationBorder({required colorBorderSide}){
+    return  OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15),
+        borderSide: BorderSide(
+            width: 1,
+            color: colorBorderSide
+        )
+    );
+  }
+}
