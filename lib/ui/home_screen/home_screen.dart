7b6ae39b12192284fa851464d18f490ffe3efa89
LTC <afnanalgabobi@gmail.com>
2025-08-22 16:27:48 +0300

    add home screen

---
+++ lib/ui/home_screen/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_app/ui/update_profile/update_profile.dart';
+import 'package:movies_app/utils/app_assets.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> widgetOptions = [
+    Placeholder(),
+    Placeholder(),
+    Placeholder(),
+    UpdateProfile(),
+  ];
+
+  var  BottomNavigationBarItems = [
+    AppAssets.homeTabUnselected,
+    AppAssets.searchTabUnselected,
+    AppAssets.browseTabUnselected,
+    AppAssets.profileTabUnselected,
+  ];
+  void _onItemTapped(int index) {
+    print(index);
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetOptions[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.blueGrey,
+        items: [
+          BottomNavigationBarItem(
+            label: 'home',
+            icon: Image.asset(BottomNavigationBarItems[0]),
+          ),
+          BottomNavigationBarItem(
+            label: 'search',
+            icon: Image.asset(BottomNavigationBarItems[1]),
+          ),
+          BottomNavigationBarItem(
+            label: 'brows',
+            icon: Image.asset(BottomNavigationBarItems[2]),
+          ),
+          BottomNavigationBarItem(
+            label: 'profile',
+            icon: Image.asset(BottomNavigationBarItems[3]),
+          ),
+        ],
+      ),
+    );
+  }
+}
